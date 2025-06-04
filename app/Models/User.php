2df8b8d327676001eee5356ca8d756fce5dbebf3
Muhammad Nathan Asy Syaiba Amien <nathasy234@gmail.com>
2025-06-04 16:44:53 +0700

    try 8

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,10 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function canAccessFilament(): bool
+{
+    return true; // atau bisa pakai logic role kalau perlu
+}
+
 }
