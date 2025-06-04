9b43d3e1882c2063a700fd27e25351e106c5e57c
Muhammad Nathan Asy Syaiba Amien <nathasy234@gmail.com>
2025-06-04 15:32:19 +0700

    try 3

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use Carbon\Carbon;
 
 class AppServiceProvider extends ServiceProvider
@@ -18,10 +19,22 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
+    // public function boot(): void
+    // {
+    //     // Set lokal dan timezone default aplikasi
+    //     Carbon::setLocale('id');
+    //     date_default_timezone_set('Asia/Jakarta');
+    // }
+
     public function boot(): void
+
     {
         // Set lokal dan timezone default aplikasi
         Carbon::setLocale('id');
         date_default_timezone_set('Asia/Jakarta');
+        
+        if (config('app.env') != 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
